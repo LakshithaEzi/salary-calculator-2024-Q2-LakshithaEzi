f5d6bc6c1a34f3a62a68be33fdf54b929c3b8252
Sameera lakshitha <123750409+LakshithaEzi@users.noreply.github.com>
2024-06-22 13:56:32 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import SalaryForm from "./pages/SalaryForm";
+
+function App() {
+  return (
+    <div className="App">
+      <SalaryForm />
+    </div>
+  );
+}
+
+export default App;
